f0d6b61d8dc86163ba1200ff2714d8e03819abe9
RaviBojja <RaviBojja@DESKTOP-NM3K16A>
2022-10-13 18:54:52 -0700

    Doing another way without getter setter method

--- Test_Calab_Codes/src/Java9/MainProgram.java
+++ Test_Calab_Codes/src/Java9/MainProgram.java
@@ -2,33 +2,20 @@ package Java9;
 //Write a program to print the names of students by creating a student class. 
 //if no name is passed while creating an object of the student class, then the n
 //name should be unknown.
-
 // Student Class which has only one variable and one function. The variable is 
 // Student name and the function is to print the name of the student.
 
 class Student {
-	String name = "Unknown";
-	public void getName() {
-		System.out.println("This student name is "+ name);
-	}
-	public void setName(String FirstName) {
-		name = FirstName;
-	}
+      String Name="Unknown";
 	}
 
 
 public class MainProgram {
 	public static void main(String[] args) {
-		Student firstStudent = new Student();
-		Student secondStudent = new Student();
-		Student thirdStudent = new Student();
-		
-		firstStudent.setName("Ram");
-		secondStudent.setName("Shanthi");
-		firstStudent.getName();
-		secondStudent.getName();
-		thirdStudent.getName();
-	}
-
-	
+      Student firstStudent = new Student();
+      Student secondStudent = new Student();
+      secondStudent.Name = "Ram";
+      System.out.println(firstStudent.Name);
+      System.out.println(secondStudent.Name);
+}
 }
